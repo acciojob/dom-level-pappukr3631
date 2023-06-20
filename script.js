35846fa77e18acef16e91e1fd9696c76d70fcb34
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-06-20 22:28:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const elements = document.getElementsByTagName("li");
+
+for(let i=0; i<elements.length; i++) {
+	if(elements[i].id == "level") {
+	alert("The level of the element is: " + (i+1));
+	}
+}
